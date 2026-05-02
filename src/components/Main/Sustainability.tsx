5ee897562d541045585906a433bce70c5de5cb95
Ankur0066-cimt <ankur.t@canimagedev.in>
2026-05-02 12:06:06 +0530

    we

--- src/components/Main/Sustainability.tsx
+++ src/components/Main/Sustainability.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sustainability: React.FC = () => {
   return (
-    <section id="what-we-do" className="bg-[#F9F6F1] py-12 md:py-18">
+    <section id="sustainability" className="bg-[#F9F6F1] py-12 md:py-18">
       <div className="font-geom max-w-8xl mx-auto px-6 md:px-10">
 
         <div className="relative bg-[#F9F6F5] rounded-3xl p-8 md:p-14 shadow-[0_4px_24px_rgba(42,59,126,0.06)] border border-[#F0EBE0]">
